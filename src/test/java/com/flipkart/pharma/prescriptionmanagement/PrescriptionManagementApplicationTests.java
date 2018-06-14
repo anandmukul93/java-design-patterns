374f843e983d91472efc1828f02384a809e57539
Mukul Anand <mukul.a@flipkart.com>
2018-06-14 16:05:52 +0530

    initial project commit

---
+++ src/test/java/com/flipkart/pharma/prescriptionmanagement/PrescriptionManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flipkart.pharma.prescriptionmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrescriptionManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
