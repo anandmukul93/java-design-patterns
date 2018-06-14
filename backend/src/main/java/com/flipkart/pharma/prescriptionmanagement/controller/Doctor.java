d50a6842e0a39234912269e47c29acdfbd598c83
Prerna Jeslani <prerna.jeslani@flipkart.com>
2018-06-14 18:23:08 +0530

    adding doctor

--- backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/controller/Doctor.java
+++ backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/controller/Doctor.java
@@ -1,20 +1,5 @@
 package com.flipkart.pharma.prescriptionmanagement.controller;
 
-import com.flipkart.pharma.prescriptionmanagement.datatypes.DoctorRequest;
-import com.flipkart.pharma.prescriptionmanagement.datatypes.DoctorResponse;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-
-@Service
-@Slf4j
 public class Doctor {
-    public DoctorResponse getDoctor(String DIN) {
-        return new DoctorResponse();
-    }
-
-    public void createDoctor(DoctorRequest doctorRequest) {
-
-    }
 
 }
--- backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/model/response/DoctorResponse.java
+++ backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/model/response/DoctorResponse.java
@@ -1,11 +1,11 @@
-package com.flipkart.pharma.prescriptionmanagement.datatypes;
+package com.flipkart.pharma.prescriptionmanagement.model.response;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class DoctorRequest {
+public class DoctorResponse {
     private String name;
     private String DIN;
     private String phone;
