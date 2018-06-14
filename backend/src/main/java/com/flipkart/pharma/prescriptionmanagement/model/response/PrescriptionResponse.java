37315b06acfb2f7f6938c53340dcf0e0fc8dbbde
Sourabh Doliya <sourabh.d@flipkart.com>
2018-06-14 18:56:32 +0530

    adding get api for prescription

--- backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/model/response/PrescriptionResponse.java
+++ backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/model/response/PrescriptionResponse.java
@@ -4,18 +4,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.flipkart.pharma.prescriptionmanagement.common.MedicineType;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 
 /**
  * Created by sourabh.d on 14/06/18.
  */
-@Data
+@Getter
+@Builder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PrescriptionResponse {
     private String pid;
     private MedicineType medicineType;
     private String medicineName;
-    private String quantity;
+    private Integer quantity;
     private String remarks;
 }
--- backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/repository/PrescriptionRepository.java
+++ backend/src/main/java/com/flipkart/pharma/prescriptionmanagement/repository/PrescriptionRepository.java
@@ -3,8 +3,11 @@ package com.flipkart.pharma.prescriptionmanagement.repository;
 import com.flipkart.pharma.prescriptionmanagement.domain.Prescription;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * Created by sourabh.d on 14/06/18.
  */
 public interface PrescriptionRepository extends CrudRepository<Prescription, Long> {
+    List<Prescription> getByPid(String pid);
 }
